ab7bae47b5546ea6755926a30ec937dd058d5584
kgori <kevin.c.gori@gmail.com>
2018-01-12 12:59:20 +0000

    Rewrote model to have separate time intervals for each sample

--- poissonProcess.stan
+++ poissonProcess.stan
@@ -1,36 +1,51 @@
 data {
     int n; // number of data points for m (samples)
     int m[n]; // number of mutations per sample in short time period (t)
-    int N; // number of data points for M
-    int M[N]; // number of mutations since origin (long time period, T)
+    int M; // number of mutations since origin of CTVT, until MRCA of all samples (long time period, T)
     
     // prior parameters:
-    real<lower=0> a;
-    real<lower=0> b;
     real<lower=0> min_permitted_lower_bound;
     real<lower=0> max_permitted_upper_bound;
     real<lower=0> expon_rate;
 }
+transformed data {
+    real PUPPY_AGE = 10.0 / 12; // age of puppy at time of sampling - 10 months
+    real PARENT_AGE = 2.5; // age of parent dog at time of sampling - 2.5 years (excluding 6 months prior to sexual maturity)
+}
 parameters {
+    real<lower=0,
+         upper=(PARENT_AGE - PUPPY_AGE)> t_mrca_raw; // raw vector later used to construct length of time (years) before present to MRCA of all samples
+    
+    vector<lower=0,
+           upper=10.0/12>[n] t; // length of time (years) before present to each sample's MRCA
+    
     real<lower=0> rate; // rate parameter of the Poisson process (mutations per unit time [year])
-    real<lower=0> t; // short time interval in which mutations are generated (years)
+    
     real<lower=min_permitted_lower_bound,
-         upper=max_permitted_upper_bound> T; // long time interval (years)
+         upper=max_permitted_upper_bound> t_origin; // length of time (years) between origin of CTVT and MRCA of all samples
+}
+transformed parameters {
+    real t_mrca = t_mrca_raw + PUPPY_AGE;
+    vector<lower=0, upper=3>[n] i = t_mrca - t; // time intervals between MRCA of all samples, and MRCAs of each sample
 }
 model {
-    // prior for 't' is Beta value ( [0,1) )
-    t ~ beta(a, b);
+    t_mrca_raw ~ exponential(18) T[, PARENT_AGE - PUPPY_AGE];  // 95% of prior weight is on 2 months prior to birth (reflecting our belief that the parent was infected in the most recent heat cycle)
+    for (j in 1:n) {
+        t[j] ~ exponential(3.6) T[, PUPPY_AGE]; // 95% of prior weight is on the interval 0 - PUPPY_AGE
+    }
     
     // prior for 'T' is Uniform ( [min_permitted, max_permitted) )
-    T ~ uniform(min_permitted_lower_bound, max_permitted_upper_bound);
+    t_origin ~ uniform(min_permitted_lower_bound, max_permitted_upper_bound);
     
     // prior on mutation rate is Exponential ( [0, inf) )
     // NB: Expectation(mutation rate) = 1/expon_rate
     rate ~ exponential(expon_rate);
     
     // likelihood of 'm' is Poisson, rate scaled by 't'
-    m ~ poisson(rate * t);
+    for (j in 1:n) {
+        m[j] ~ poisson(rate * i[j]);
+    }
     
     // likelihood of 'M' is Poisson, rate scaled by 'T'
-    M ~ poisson(rate * T);
+    M ~ poisson(rate * t_origin);
 }
--- runPoissonProcess.R
+++ runPoissonProcess.R
@@ -1,56 +1,15 @@
 require(rstan)
-
-poisson_model <- "
-data {
-    int n; // number of data points for m (samples)
-    int m[n]; // number of mutations per sample in short time period (t)
-    int N; // number of data points for M
-    int M[N]; // number of mutations since origin (long time period, T)
-    
-    // prior parameters:
-    real<lower=0> a;
-    real<lower=0> b;
-    real<lower=0> min_permitted_lower_bound;
-    real<lower=0> max_permitted_upper_bound;
-    real<lower=0> expon_rate;
-}
-parameters {
-    real<lower=0> rate; // rate parameter of the Poisson process (mutations per unit time [year])
-    real<lower=0> t; // short time interval in which mutations are generated (years)
-    real<lower=min_permitted_lower_bound,
-    upper=max_permitted_upper_bound> T; // long time interval (years)
-}
-model {
-    // prior for 't' is Beta value ( [0,1) )
-    t ~ beta(a, b);
-    
-    // prior for 'T' is Uniform ( [min_permitted, max_permitted) )
-    T ~ uniform(min_permitted_lower_bound, max_permitted_upper_bound);
-    
-    // prior on mutation rate is Exponential ( [0, inf) )
-    // NB: Expectation(mutation rate) = 1/expon_rate
-    rate ~ exponential(expon_rate);
-    
-    // likelihood of 'm' is Poisson, rate scaled by 't'
-    m ~ poisson(rate * t);
-    
-    // likelihood of 'M' is Poisson, rate scaled by 'T'
-    M ~ poisson(rate * T);
-}
-"
+setwd("~/code/stan/poissonProcess")
 
 # Compile the model
-my_model <- rstan::stan_model(model_code = poisson_model,
+my_model <- rstan::stan_model(file = "poissonProcess.stan",
                               model_name = "CTVT poisson process")
 
 # List of model parameters + input data
 dat <- list(
     n = 2,  # number of post-MRCA mutation estimates
-    N = 1,  # number of pre-MRCA mutation estimates
     m = c(23, 27),  # post-MRCA mutation estimates
-    M = array(221370),  # pre-MRCA mutation estimates (use array to pass a single value)
-    a = 1,  # parameter a of beta prior on 't'
-    b = 1,  # parameter b of beta prior on 't'
+    M = 221370,  # pre-MRCA mutation estimates (use array to pass a single value)
     min_permitted_lower_bound = 200,  # lower bound on uniform prior on 'T'
     max_permitted_upper_bound = 25000,  # upper bound on uniform prior on 'T'
     expon_rate = 0.025  # parameter of exponential prior on mean mutation rate (corresponds to ~= 40)
@@ -60,10 +19,14 @@ dat <- list(
 trace <- rstan::sampling(my_model, data = dat, iter = 50000, chains = 4)
 
 # List parameter estimates
-summary(trace, prob = c(0.025, 0.975))
+summary(trace, prob = c(0.025, 0.975))$summary
 
 # Plot some stuff
 require(bayesplot)
-bayesplot::mcmc_areas(as.array(trace), pars = "t", prob = .95)
-bayesplot::mcmc_areas(as.array(trace), pars = "T", prob = .95)
-bayesplot::mcmc_areas(as.array(trace), pars = "rate", prob = .95)
+bayesplot::mcmc_areas(as.array(trace), pars = "t_mrca", prob = .95) + ggplot2::labs(title = "t_mrca")
+bayesplot::mcmc_areas(as.array(trace), pars = "t[1]", prob = .95) + ggplot2::labs(title = "t_608")
+bayesplot::mcmc_areas(as.array(trace), pars = "t[2]", prob = .95) + ggplot2::labs(title = "t_609")
+bayesplot::mcmc_areas(as.array(trace), pars = "t_origin", prob = .95) + ggplot2::labs(title = "Time to CTVT origin (years)"); #ggsave("timeToOrigin.pdf")
+bayesplot::mcmc_areas(as.array(trace), pars = "rate", prob = .95) + ggplot2::labs(title = "Mutation rate (total substitutions per year)"); #ggsave("mutationRate.pdf")
+bayesplot::mcmc_areas(as.array(trace), pars = "i[1]", prob = .95) + ggplot2::labs(title = "i_608")
+bayesplot::mcmc_areas(as.array(trace), pars = "i[2]", prob = .95) + ggplot2::labs(title = "i_609")
