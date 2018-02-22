254248a1befacf01f16c2e0ab69e92ec01a1987e
Kevin Gori <kevin.c.gori@gmail.com>
2018-02-22 17:28:30 +0000

    Remove hard-coded path

--- runPoissonProcess.R
+++ runPoissonProcess.R
@@ -1,5 +1,33 @@
 require(rstan)
-setwd("~/code/stan/poissonProcess")
+require(bayesplot)
+
+# Get the path of this script - the stan file should be here too
+# https://github.com/molgenis/molgenis-pipelines.wiki.git
+LocationOfThisScript = function() # Function LocationOfThisScript returns the location of this .R script (may be needed to source other files in same dir)
+{
+	this.file = NULL
+	# This file may be 'sourced'
+	for (i in -(1:sys.nframe())) {
+		if (identical(sys.function(i), base::source)) this.file = (normalizePath(sys.frame(i)$ofile))
+	}
+
+	if (!is.null(this.file)) return(dirname(this.file))
+
+	# But it may also be called from the command line
+	cmd.args = commandArgs(trailingOnly = FALSE)
+	cmd.args.trailing = commandArgs(trailingOnly = TRUE)
+	cmd.args = cmd.args[seq.int(from=1, length.out=length(cmd.args) - length(cmd.args.trailing))]
+	res = gsub("^(?:--file=(.*)|.*)$", "\\1", cmd.args)
+
+	# If multiple --file arguments are given, R uses the last one
+	res = tail(res[res != ""], 1)
+	if (0 < length(res)) return(dirname(res))
+
+	# Both are not the case. Maybe we are in an R GUI?
+	return(NULL)
+}
+current.dir <- normalizePath(LocationOfThisScript())
+setwd(current.dir)
 
 # Compile the model
 my_model <- rstan::stan_model(file = "poissonProcess.stan",
@@ -22,7 +50,6 @@ trace <- rstan::sampling(my_model, data = dat, iter = 50000, chains = 4)
 summary(trace, prob = c(0.025, 0.975))$summary
 
 # Plot some stuff
-require(bayesplot)
 bayesplot::mcmc_areas(as.array(trace), pars = "t_mrca", prob = .95) + ggplot2::labs(title = "t_mrca")
 bayesplot::mcmc_areas(as.array(trace), pars = "t[1]", prob = .95) + ggplot2::labs(title = "t_608")
 bayesplot::mcmc_areas(as.array(trace), pars = "t[2]", prob = .95) + ggplot2::labs(title = "t_609")
